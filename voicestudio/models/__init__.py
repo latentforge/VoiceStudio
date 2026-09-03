2d0171d4f9d4886b4bb31dd1d8ac809d886e2a87
b-re-w <irack000@gmail.com>
2026-09-03 09:19:29 +0900

    Feat: Register Dia2 And Breeze TTS 2 In The Models Package
    Both migrations landed in 26aee709 and 2cc89500 but could not wire
    themselves up, since agents working in parallel are kept out of this
    shared file. Importing the package resolves both, so their Auto class
    registrations now run.

--- voicestudio/models/__init__.py
+++ voicestudio/models/__init__.py
@@ -1,4 +1,6 @@
+from .breeze_tts import *
 from .dia import *
+from .dia2 import *
 from .higgs_tts2 import *
 from .higgs_tts3 import *
 from .qwen3_tts import *
---
+++ voicestudio/models/cosyvoice_v1/__init__.py
@@ -0,0 +1,4 @@
+from .configuration_cosyvoice_v1 import *
+from .generation_cosyvoice_v1 import *
+from .modeling_cosyvoice_v1 import *
+from .processing_cosyvoice_v1 import *
